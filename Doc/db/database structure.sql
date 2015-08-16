c64afcf73bd60ea96a2f4aaf2972f2b7fd3dcab4
tanyixiu <yixiu.tan@gmail.com>
2015-08-16 13:22:31 +0800

    sql modify

--- Doc/db/database structure.sql
+++ Doc/db/database structure.sql
@@ -15,63 +15,19 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`scrumer` /*!40100 DEFAULT CHARACTER SET
 
 USE `scrumer`;
 
-/*Table structure for table `logmessage` */
-
-DROP TABLE IF EXISTS `logmessage`;
-
-CREATE TABLE `logmessage` (
-  `id` varchar(36) NOT NULL,
-  `description` varchar(500) NOT NULL,
-  `actiontime` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Table structure for table `photo` */
-
-DROP TABLE IF EXISTS `photo`;
-
-CREATE TABLE `photo` (
-  `id` varchar(36) NOT NULL,
-  `url` varchar(200) NOT NULL,
-  `md5` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 /*Table structure for table `project` */
 
 DROP TABLE IF EXISTS `project`;
 
 CREATE TABLE `project` (
   `id` varchar(36) NOT NULL,
+  `teamid` varchar(36) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `photoid` varchar(36) NOT NULL DEFAULT '',
-  `teamid` varchar(36) NOT NULL,
-  `description` varchar(200) NOT NULL DEFAULT '',
+  `description` varchar(500) NOT NULL DEFAULT '',
+  `creatorid` varchar(36) NOT NULL DEFAULT '',
+  `createtime` varchar(50) NOT NULL DEFAULT '',
+  `state` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Table structure for table `rel_story_user` */
-
-DROP TABLE IF EXISTS `rel_story_user`;
-
-CREATE TABLE `rel_story_user` (
-  `id` varchar(36) NOT NULL,
-  `storyid` varchar(36) NOT NULL,
-  `userid` varchar(36) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `storyid` (`storyid`,`userid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Table structure for table `rel_team_user` */
-
-DROP TABLE IF EXISTS `rel_team_user`;
-
-CREATE TABLE `rel_team_user` (
-  `id` varchar(36) NOT NULL,
-  `teamid` varchar(36) NOT NULL,
-  `userid` varchar(36) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `teamid` (`teamid`,`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Table structure for table `story` */
@@ -80,24 +36,30 @@ DROP TABLE IF EXISTS `story`;
 
 CREATE TABLE `story` (
   `id` varchar(36) NOT NULL,
-  `groupid` varchar(36) NOT NULL,
   `projectid` varchar(36) NOT NULL,
-  `teamid` varchar(36) NOT NULL,
-  `description` varchar(500) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(500) NOT NULL DEFAULT '',
+  `parentid` varchar(36) NOT NULL DEFAULT '0',
   `state` int(11) NOT NULL DEFAULT '0',
+  `priority` int(11) NOT NULL DEFAULT '1',
+  `creatorid` varchar(36) NOT NULL DEFAULT '',
+  `createtime` varchar(50) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Table structure for table `storygroup` */
+/*Table structure for table `story_user` */
 
-DROP TABLE IF EXISTS `storygroup`;
+DROP TABLE IF EXISTS `story_user`;
 
-CREATE TABLE `storygroup` (
+CREATE TABLE `story_user` (
   `id` varchar(36) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `projectid` varchar(36) NOT NULL,
-  `teamid` varchar(36) NOT NULL,
-  PRIMARY KEY (`id`)
+  `storyid` varchar(36) NOT NULL,
+  `userid` varchar(36) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '0',
+  `starttime` varchar(30) NOT NULL DEFAULT '',
+  `finishtime` varchar(30) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `storyid` (`storyid`,`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Table structure for table `team` */
@@ -107,9 +69,23 @@ DROP TABLE IF EXISTS `team`;
 CREATE TABLE `team` (
   `id` varchar(36) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `photoid` varchar(36) NOT NULL DEFAULT '',
-  `description` varchar(200) NOT NULL DEFAULT '',
+  `description` varchar(500) NOT NULL DEFAULT '',
+  `creatorid` varchar(36) NOT NULL DEFAULT '',
+  `createtime` varchar(50) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `team_user` */
+
+DROP TABLE IF EXISTS `team_user`;
+
+CREATE TABLE `team_user` (
+  `id` varchar(36) NOT NULL,
+  `teamid` varchar(36) NOT NULL,
+  `userid` varchar(36) NOT NULL,
+  `jointime` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `teamid` (`teamid`,`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Table structure for table `user` */
@@ -120,7 +96,13 @@ CREATE TABLE `user` (
   `id` varchar(36) NOT NULL,
   `name` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `photoid` varchar(36) NOT NULL DEFAULT '',
+  `tel` varchar(20) NOT NULL DEFAULT '',
+  `email` varchar(50) NOT NULL DEFAULT '',
+  `qq` varchar(50) NOT NULL DEFAULT '',
+  `weibo` varchar(50) NOT NULL DEFAULT '',
+  `weixin` varchar(50) NOT NULL DEFAULT '',
+  `registertime` varchar(50) NOT NULL DEFAULT '',
+  `photourl` varchar(200) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
